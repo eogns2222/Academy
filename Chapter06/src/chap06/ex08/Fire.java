1de050a783f306c5b3baaf2373c9a656c1d5bb94
eogns22222 <eogns22222@gmail.com>
2024-01-29 16:07:42 +0900

    This is Chapter06 commit

---
+++ Chapter06/src/chap06/ex08/Fire.java
@@ -0,0 +1,12 @@
+package chap06.ex08;
+
+public class Fire extends Spell {
+	@Override
+	public String casting() {
+		return "화염" + super.casting();
+	}
+			
+		
+	
+	
+}
---
+++ Chapter06/src/chap06/ex08/Spell.java
@@ -0,0 +1,16 @@
+package chap06.ex08;
+
+public class Spell {
+	
+	Spell spell;
+	public String casting() {
+		return "마법을 시전한다.";
+		
+		
+		
+		
+	}
+		
+	
+
+}
